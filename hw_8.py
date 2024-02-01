8f900210a50e8dbeccaa84527a4e1cd50ff8afde
nursultanaid <atakexs@gmail.com>
2024-02-01 23:59:52 +0600

    first commit

--- hw_8.py
+++ hw_8.py
@@ -1,97 +1,1 @@
-import sqlite3
-
-def create_connection(db_name):
-    conn = None
-    try:
-        conn = sqlite3.connect(db_name)
-    except sqlite3.Error as e:
-        print(e)
-    return conn
-
-def create_table(conn, sql):
-    try:
-        cursor = conn.cursor()
-        cursor.execute(sql)
-    except sqlite3.Error as e:
-        print(e)
-    return conn
-
-def insert_data(conn, sql, data):
-    try:
-        cursor = conn.cursor()
-        cursor.executemany(sql, data)
-        conn.commit()
-    except sqlite3.Error as e:
-        print(e)
-
-sql_create_students_table = '''
-CREATE TABLE students(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    first_name TEXXT NOT NULL,    
-    last_name TEXT NOT NULL,
-    city_id INTEGER,    
-    FOREIGN KEY (city_id) REFERENCES cities (id)
-    )'''
-
-sql_to_create_cities_table = '''
-    CREATE TABLE IF NOT EXISTS cities(    
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,    
-    area REAL DEFAULT 0,
-    country_id INTEGER,    
-    FOREIGN KEY (country_id) REFERENCES countries (id)
-    )'''
-
-sql_to_create_countries_table = '''
-    CREATE TABLE IF NOT EXISTS countries(    
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL    )
-'''
-
-sql_insert_students_data = '''
-INSERT INTO students (first_name, last_name, city_id) VALUES (?,?,?)
-'''
-
-sql_insert_countries_data = '''
-INSERT INTO countries (title) VALUES (?)
-'''
-
-sql_insert_cities_data = '''
-INSERT INTO cities (title, country_id) VALUES (?, ?)
-'''
-
-cities_data = [('New-York', 1), ('Los_Angeles', 1), ('Paris', 2), ('Toulouse', 2), ('Milan', 3), ('Rome', 3), ('Venice', 3)]
-countries_data = [('USA',), ('France',), ('Italy',)]
-students_data = [
-    ('Nursultan', 'Aidarbekov', 1),
-    ('John', 'Doe', 1),
-    ('Mike', 'John', 2),
-    ('Mitt', 'Rose', 3),
-    ('Will', 'Smith', 2),
-    ('Amber', 'Miles', 3),
-    ('Close', 'Miroslav', 2),
-    ('Ali', 'Owairan', 2),
-    ('Jenny', 'Ben', 2),
-    ('Wilson', 'Son', 1),
-    ('Nui', 'Anderson', 3),
-    ('Lois', 'Vuitton', 2),
-    ('Peter', 'Griffin', 1),
-    ('Rui', 'Patrik', 1),
-    ('Sam', 'Dorris', 3),
-    ('Michael', 'Cors', 1),
-]
-
-
-connection = create_connection('hw_8.db')
-if connection is not None:
-    print('Successfully connected')
-    # create_table(connection, sql_create_students_table)
-    # insert_data(connection, sql_insert_students_data, students_data)
-
-    # create_table(connection, sql_to_create_countries_table)
-    # insert_data(connection, sql_insert_countries_data, countries_data)
-
-    # create_table(connection, sql_to_create_cities_table)
-    # insert_data(connection, sql_insert_cities_data, cities_data)
-
-    connection.close()
+import sqlite3def create_connection(db_name):    conn = None    try:        conn = sqlite3.connect(db_name)    except sqlite3.Error as e:        print(e)    return conndef create_countries_table(conn):    cursor = conn.cursor()    cursor.execute('''CREATE TABLE IF NOT EXISTS countries                       (id INTEGER PRIMARY KEY AUTOINCREMENT,                       title TEXT NOT NULL)''')    conn.commit()def insert_countries(conn):    cursor = conn.cursor()    cursor.execute("INSERT INTO countries (title) VALUES (?)", ('USA',))    cursor.execute("INSERT INTO countries (title) VALUES (?)", ('Germany',))    cursor.execute("INSERT INTO countries (title) VALUES (?)", ('China',))    conn.commit()def create_cities_table(conn):    cursor = conn.cursor()    cursor.execute('''CREATE TABLE IF NOT EXISTS cities                       (id INTEGER PRIMARY KEY AUTOINCREMENT,                       title TEXT NOT NULL,                       area FLOAT DEFAULT 0,                       country_id INTEGER,                      FOREIGN KEY (country_id) REFERENCES countries(id))''')    conn.commit()def insert_cities(conn):    cursor = conn.cursor()    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('New York', 1223, 1))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Berlin', 891, 2))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Beijing', 16410, 3))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Los Angeles', 1302, 1))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Chicago', 606, 1))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Hamburg', 755, 2))    cursor.execute("INSERT INTO cities (title, area, country_id) VALUES (?, ?, ?)", ('Shanghai', 6340, 3))    conn.commit()def create_students_table(conn):    cursor = conn.cursor()    cursor.execute('''CREATE TABLE IF NOT EXISTS students                       (id INTEGER PRIMARY KEY AUTOINCREMENT,                       first_name TEXT NOT NULL,                       last_name TEXT NOT NULL,                       city_id INTEGER,                      FOREIGN KEY (city_id) REFERENCES cities(id))''')    conn.commit()def insert_students(conn):    cursor = conn.cursor()    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Regina', 'Martin', 1))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Gerald', 'Richardson', 2))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Dawn', 'Jones', 3))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Pamela', 'Jones', 4))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Wesley', 'Glover', 1))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Charles', 'West', 1))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Lois', 'Carter', 4))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Margaret', 'Perez', 2))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Grace', 'Lane', 3))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Timothy', 'Hill', 2))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Thomas', 'Foster', 4))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Martha', 'Smith', 3))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Shirley', 'Howard', 4))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Daniel', 'Johnson', 1))    cursor.execute("INSERT INTO students (first_name, last_name, city_id) VALUES (?, ?, ?)", ('Shirley', 'Clark', 1))    conn.commit()def get_cities(conn):    cursor = conn.cursor()    cursor.execute("SELECT title FROM cities")    cities = cursor.fetchall()    return citiesdef get_students_by_city(conn, city_id):    cursor = conn.cursor()    cursor.execute("SELECT students.first_name, students.last_name, countries.title, cities.title, cities.area "                   "FROM students "                   "JOIN cities ON students.city_id = cities.id "                   "JOIN countries ON cities.country_id = countries.id "                   "WHERE cities.id = ?", (city_id,))    students = cursor.fetchall()    return studentsdef print_cities(cities):    print("List of cities:")    for city in cities:        print(city[0])def print_students(students):    print("Information about students in the selected city:")    for student in students:        print("Name:", student[0])        print("Last name:", student[1])        print("Country:", student[2])        print("City:", student[3])        print("Area of the city:", student[4])        print('------------------------------')def main():    conn = create_connection('students.db')    if conn is not None:        print('Successfully connected to DB!')    create_countries_table(conn)    insert_countries(conn)    create_cities_table(conn)    insert_cities(conn)    create_students_table(conn)    insert_students(conn)    cities = get_cities(conn)    print_cities(cities)    while True:        city_id = int(input("You can display a list of students "                            "by the selected city id from the list of cities below, "                            "\nto exit the program, enter 0: "))        if city_id == 0:            break        students = get_students_by_city(conn, city_id)        print_students(students)    conn.close()if __name__ == '__main__':    main()
